e4c0a6bf330a84fae2b1a5fbbb5d35110b4dfb33
alexds <alexds@ciandt.com>
2020-01-17 12:43:41 -0300

    Configuring for code generator by swagger

---
+++ src/main/java/com/ciandt/scheduletasks/ScheduleTasksApplication.java
@@ -0,0 +1,13 @@
+package com.ciandt.scheduletasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciandt/scheduletasks/ScheduleTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciandt.scheduletasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
